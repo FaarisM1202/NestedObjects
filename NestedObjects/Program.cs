1734294ad8d9c7f47d066c038abd47b29da775c5
Faaris Mohamed <mohamed8876@students.cptc.edu>
2023-01-17 15:54:21 -0800

    Used my Console.App

--- NestedObjects/Program.cs
+++ NestedObjects/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NestedObjects;
+
+
+using NestedObjects
+Advisor compSciAdvisor = new();
+{
+    Email = "randomemail123@gmail.com",
+    FullName = "John Smith",
+    OfficeLocation = "B17 Rm 150"
+};
+
+Student stu1 = new();
+{
+    FirstName = "Jimmy",
+    LastName = "Neutron",
+    DateOfBirth = new DateTime(1999, 5, 14),
+    AssignedAdvisor = compSciAdvisor,
+    PhoneNumber = "12345678912",
+    SchoolEmail = "jimmy.neutron123@gmail.com"
+};
+Console.WriteLine($"Student: {stu1.FirstName} has {stu1.AssignedAdvisor.FullName}");
