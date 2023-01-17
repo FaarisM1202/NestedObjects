fc630260a881ad8c933a530a3155bf879d76304e
Faaris Mohamed <mohamed8876@students.cptc.edu>
2023-01-17 15:18:18 -0800

    Created Student and Advisor Classes

---
+++ NestedObjects/Advisor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents a college advisor
+    /// </summary>
+    public class Advisor
+    {
+        /// <summary>
+        /// The legal first and last name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Advisor's work email
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// The Advisor's room number and location in the building
+        /// </summary>
+        public string OfficeLocation { get; set; }
+    }
+}
---
+++ NestedObjects/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents individual college students
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// Legal first name
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Legal last name
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Assigned school email address
+        /// </summary>
+        public string SchoolEmail { get; set; }
+        /// <summary>
+        /// Assigned phone number
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+    }
+}
